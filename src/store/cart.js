fab6a233d70f7b0d0276c9cf4ec05177fd96611e
HawraaYounes <hawraayounes0@gmail.com>
2024-08-28 05:56:52 +0300

    updated add function

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,13 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialCartState = { showCart: false, items: [] };
+const initialCartState = { showCart: false, items: [], totalQuantity: 0 };
 
 const cartSlice = createSlice({
   name: "Cart",
   initialState: initialCartState,
   reducers: {
-    add(state,action) {
-      state.items=[...state.items, action.payload]
+    add(state, action) {
+      const newItem = action.payload;
+      const exisitingItem = state.items.find((item) => item.id === newItem.id);
+      if (!exisitingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          title: newItem.title,
+          price: newItem.price,
+          totalPrice: newItem.totalPrice,
+          quantity: 1,
+        });
+      } else {
+        exisitingItem.quantity++;
+        exisitingItem.totalPrice = exisitingItem.totalPrice + newItem.price;
+      }
     },
     toggle(state) {
       state.showCart = !state.showCart;
