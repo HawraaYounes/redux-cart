5d19568e25a97ae566a117dcdf3479c6acb4584b
HawraaYounes <hawraayounes0@gmail.com>
2024-08-28 11:14:51 +0300

    dispatched notification state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.cart.showCart);
   const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
     const sendCartData = async () => {
+      dispatch(
+        uiActions.sendNotification({
+          status: "pending",
+          title: "Sending..",
+          message: "Sending Cart Data.",
+        })
+      );
       const response = await fetch(
         "https://react-redux-5568d-default-rtdb.firebaseio.com/cart.json",
         {
@@ -20,10 +29,26 @@ function App() {
       if (!response.ok) {
         throw new Error("Sending Cart Data Failed!");
       }
-      const data = response.json();
-      
+      const responseData = response.json();
+
+      dispatch(
+        uiActions.sendNotification({
+          status: "success",
+          title: "Success",
+          message: "Data Sent Succesfully!",
+        })
+      );
     };
-  }, [cart]);
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.sendNotification({
+          status: "pending",
+          title: "Sending..",
+          message: "Sending Cart Data.",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
     <Layout>
